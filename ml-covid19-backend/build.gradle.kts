883ab8ba66773984a34d143b2cc223c5d33ee60e
Vya4eslav111 <vya4eslav.seleznev@gmail.com>
2021-06-23 17:03:15 +0300

    #89 structure for migration

--- ml-covid19-backend/build.gradle.kts
+++ ml-covid19-backend/build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "2.4.4"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    id("org.flywaydb.flyway") version "7.10.0"
     kotlin("jvm") version "1.4.31"
     kotlin("plugin.spring") version "1.4.31"
 }
@@ -21,8 +22,16 @@ dependencies {
     implementation ("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation ("com.oracle.database.jdbc:ojdbc8:19.6.0.0")
+    implementation ("org.flywaydb:flyway-core")
+    implementation ("org.postgresql:postgresql:9.2-1003-jdbc4")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+flyway {
+    url = "{jdbc:postgresql://HOST:PORT/DB}"
+    user = "{USER}"
+    password = "{PASSWORD}"
+    locations = arrayOf("filesystem:src/main/resources/db/migration")
 }
 
 tasks.withType<KotlinCompile> {
